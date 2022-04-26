90ab584770c03f7b854d3be87342bcabb15349af
PrestonFawcett <83142528+PrestonFawcett@users.noreply.github.com>
2022-04-26 13:46:15 -0700

    Base files

---
+++ rubik_solver.py
@@ -0,0 +1,8 @@
+from rubik.cube import Cube
+c = Cube("OOOOOOOOOYYYWWWGGGBBBYYYWWWGGGBBBYYYWWWGGGBBBRRRRRRRRR")
+print(c)
+print(c.is_solved())
+
+cube = Cube("YGGRWOGYGBWWOORWGYRYOBBYBOWBGBORROOWBGRBYGYGWRWYWYRBRO")
+print(cube)
+print(cube.is_solved())
